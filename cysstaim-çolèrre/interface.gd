b7907dd87578e15a7984520fe264056782a354b7
Gaudet <gaudet@Gaudets-MacBook-Air.local>
2026-04-30 10:45:08 -0400

    correction git

--- cysstaim-çolèrre/interface.gd
+++ cysstaim-çolèrre/interface.gd
@@ -1,41 +1,35 @@
-extends Control 
+extends Control
 class_name Interface
-
-@export_group("Simulation")
-@export var lune_1 : Lune
-@export var lune_2 : Lune
+#@export_group("Simulation")
+#@export var lune_1 : Lune
+#@export var lune_2 : Lune
 
 @export_group("Textes")
-@export var rayon_1 : Label
-@export var vitesse_1 : Label
-@export var rayon_2 : Label
-@export var vitesse_2 : Label
-
-@export var distance : Label
-@export var point : Label
-
+@export var nom_astre : Label
+@export var masse_du_corps : Label
+@export var vitesse_au_perihelie : Label
+@export var excentricite_de_lorbite : Label
+@export var periode_revolution_autour_soleil : Label
+@export var periode_rotation_sur_soi : Label
 @export var vitesse_simulation : Label
 
 
 func _process(delta:float) -> void:
-	rayon_1.text = format_scientifique(lune_1.r_1.length())
-	vitesse_1.text = format_scientifique(lune_1.v_1.length())
-	rayon_2.text = format_scientifique(lune_2.r_1.length())
-	vitesse_2.text = format_scientifique(lune_2.v_1.length())
-	distance.text = format_scientifique(abs(lune_1.r_1.length() - lune_2.r_1.length()))
-	
-	if lune_1.r_1.length() < lune_2.r_1.length():
-		point.text = "Point rouge"
-	else:
-		point.text = "Point bleu"
+	if astre_clique != null:
+		nom_astre.text = str(astre_clique.var)
+		masse_du_corps.text = str(astre_clique.var)
+		vitesse_au_perihelie.text = str(astre_clique.var)
+		excentricite_de_lorbite.text = str(astre_clique.var)
+		periode_revolution_autour_soleil.text = str(astre_clique.var)
+		periode_rotation_sur_soi.text = str(astre_clique.var)
+	vitesse_simulation.text = str(astre_clique.var)
 
 func changer_mode_pause(mode_pause : bool) -> void:
 	"""Change l'état de pause à chacun des astres
 	
 	Paramètre :
 	etat_pause -- est-ce que la simulation est en pause ou non """
-	lune_1.mettre_en_pause(mode_pause)
-	lune_2.mettre_en_pause(mode_pause)
+	#lune_1.mettre_en_pause(mode_pause)
 
 
 func format_scientifique(valeur : float) -> String:
@@ -53,7 +47,7 @@ func format_scientifique(valeur : float) -> String:
 
 
 func _on_h_slider_value_changed(valeur: float) -> void:
-	"""Modifie la l'échelle de temps de la simulation lorsque le slider est utilisé
+	"""Modifie l'échelle de temps de la simulation lorsque le slider est utilisé
 	
 	Parametre:
 	valeur -- la valeur du multiplicateur de temps
@@ -61,6 +55,6 @@ func _on_h_slider_value_changed(valeur: float) -> void:
 	Retour:
 	une chaîne de caractères représentant ce nombre
 	"""
-	lune_1.echelle_temps = valeur
-	lune_2.echelle_temps = valeur
-	vitesse_simulation.text = "x" + "%.1f" % valeur
+	#lune_1.echelle_temps = valeur
+	#lune_2.echelle_temps = valeur
+	vitesse_simulation.text = "Chaque seconde vaut" + "%.1f" % valeur + "mois terrestres"
